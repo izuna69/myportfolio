46094a724b0c86fd5deced382eeaee3ec670a4f7
izuna69 <94025204+izuna69@users.noreply.github.com>
2026-01-27 12:47:41 +0900

    feat: add initial profile data constants.

--- src/data/stacks/constants.ts
+++ src/data/stacks/constants.ts
@@ -10,7 +10,7 @@ export const PROFILE_DATA = {
     discord: "https://discord.gg/KMcc8jpG",
   },
   languages: [
-    { name: "Korean", value: 90, color: "#4ade80" },
+    { name: "Korean", value: 100, color: "#4ade80" },
     { name: "Japanese", value: 70, color: "#4ade80" },
     { name: "English", value: 50, color: "#4ade80" },
   ],
